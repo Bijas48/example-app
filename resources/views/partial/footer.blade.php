21b74a194b9cba7203de6f770ec6e81a53d24637
Bijas48 <adriankwk9m@gmail.com>
2023-05-21 19:21:57 +0700

    perubahan footer

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -1,34 +1,45 @@
-<footer class="text-center text-lg-start" style="background-color: #73777B">
-    <div class="container p-4">
-        <div class="row">
-            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                <div class="square">
-                    <div class="circle">
-                      <a href="#" class="circle-content">
-                        <p>Team Us</p>
-                      </a>
-                    </div>
-                  </div>
-            </div>
-
-            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                {{-- <h5 class="text-uppercase">Contact Us</h5>
-                <ul class="list-unstyled mb-0">
-                    <li>
-                        <a href="#!" class="text-dark">Address: 123 Main St, City, State, Country</a>
-                    </li>
-                    <li>
-                        <a href="#!" class="text-dark">Phone: 555-555-5555</a>
-                    </li>
-                    <li>
-                        <a href="#!" class="text-dark">Email: info@website.com</a>
-                    </li>
-                </ul> --}}
+<footer>
+    <div class="row justify-content-around mb-0 pt-5 pb-0 bg-dark">
+        <div class="col-11">
+            <div class="row justify-content-center">
+                <div class="col-md-3 col-12 font-italic align-items-center mt-md-3 mt-4">
+                    <h5><span><img src="https://extendthemes.com/wp-content/uploads/2018/06/xbox-green-min.png" class="img-fluid mb-1"></span><b class="text-light"> Key<span class="text-muted"> Pass</span></b></h5>
+                    <p class="social mt-md-3 mt-2">
+                        <span class="text-white"><i class="fa fa-facebook" aria-hidden="true"></i></span>
+                        <span class="text-white"><i class="fa fa-instagram" aria-hidden="true"></i></span>
+                        <span class="text-white"><i class="fa fa-tiktok" aria-hidden="true"></i></span>
+                        <span class="text-white"><i class="fa fa-twitter" aria-hidden="true"></i></span>
+                    </p>
+                    <small class="copy-rights cursor-pointer text-light">&#9400; 2019 EasyGo Digital Technologies</small><br>
+                    <small class="text-light">Copyright. All Rights Reserved.</small>
+                </div>
+                <div class="col-md-3 col-12 my-sm-0 mt-5">
+                    <ul class="list-unstyled">
+                        <li class="mt-md-3 mt-4">COMPANY</li>
+                        <li>IMPRINT</li>
+                        <li>CONTACT</li>
+                        <li>PRICING</li>
+                        <li>CAREERS</li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-12 my-sm-0 mt-5">
+                    <ul class="list-unstyled">
+                        <li class="mt-md-3 mt-4">ABOUT US</li>
+                        <li>OUR TEAM</li>
+                        <li>ABOUT</li>
+                        <li>SUPPORT</li>
+                        <li>MEDIA KIT</li>
+                    </ul>
+                </div>
+                <div class="col-xl-auto col-md-3 col-12 my-sm-0 mt-5">
+                    <ul class="list-unstyled">
+                        <li class="mt-md-3 mt-4">BLOG</li>
+                        <li>LATEST BLOG</li>
+                        <li>POPULAR POST</li>
+                        <li>SEARCH</li>
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
-
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        © 2023
     </div>
 </footer>
